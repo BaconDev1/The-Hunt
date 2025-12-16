3d076a9881ae47c100ed7bd3f4da94d49501e813
BaconDev1 <120346945+BaconDev1@users.noreply.github.com>
2025-12-16 14:46:19 -0500

    Movement

---
+++ The Hunt/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class PlayerLook : MonoBehaviour
+{
+
+    public Transform PlayerCamera;
+    public Vector2 Sensitivities;
+
+    private Vector2 XYRotation;
+
+    [Header("Camera Tilt")]
+    [SerializeField] private float tiltAmount;
+    [SerializeField] private float tiltStartSpeed;
+    [SerializeField] private float tiltEndSpeed;
+
+    private float currentTilt = 0f;
+    private float targetTilt = 0f;
+    public bool RotationEnabled = true;       // Normal mouse rotation
+    public bool IgnoreCamera = false;         // Completely stops writing to camera
+
+    public bool canLook = true;
+
+    public void SetCanMove(bool value)
+    {
+        // look doesn't handle movement, ignore this
+    }
+
+    public void SetCanLook(bool value)
+    {
+        canLook = value;
+
+       
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!canLook)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            return;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        if (RotationEnabled)
+        {
+            Vector2 MouseInput = new Vector2 { x = Input.GetAxis("Mouse X"), y = Input.GetAxis("Mouse Y") };
+
+            XYRotation.x -= MouseInput.y * Sensitivities.y;
+            XYRotation.y += MouseInput.x * Sensitivities.x;
+
+            XYRotation.x = Mathf.Clamp(XYRotation.x, -90f, 90f);
+
+            transform.eulerAngles = new Vector3(0f, XYRotation.y, 0);
+          
+            // Only update camera if we're not ignoring it
+            if (!IgnoreCamera)
+                PlayerCamera.localEulerAngles = new Vector3(XYRotation.x, 0f, currentTilt);
+
+            
+        }
+
+        CameraTilt();
+    }
+    public void ForceSetRotation(Quaternion worldRotation)
+    {
+        // Convert to local Euler relative to the player body
+        Vector3 euler = worldRotation.eulerAngles;
+
+        // Convert 0–360 Unity angles into −180…180 range
+        float pitch = euler.x > 180f ? euler.x - 360f : euler.x;
+        float yaw = euler.y;
+
+        XYRotation.x = Mathf.Clamp(pitch, -90f, 90f);
+        XYRotation.y = yaw;
+    }
+    private void CameraTilt()
+    {
+        bool leftStrafe = Input.GetKey(KeyCode.A);
+        bool rightStrafe = Input.GetKey(KeyCode.D);
+
+        if(leftStrafe && !rightStrafe)
+        {
+            targetTilt = tiltAmount;
+        }else if (rightStrafe && !leftStrafe)
+        {
+            targetTilt = -tiltAmount;
+        }
+        else
+        {
+            targetTilt = 0f;
+        }
+
+        float smoothTilt;
+        if(targetTilt == 0)
+        {
+            smoothTilt = tiltEndSpeed;
+        }
+        else
+        {
+            smoothTilt = tiltStartSpeed;
+        }
+
+        currentTilt = Mathf.Lerp(currentTilt, targetTilt, smoothTilt * Time.deltaTime);
+    }
+}
---
+++ The Hunt/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+
+public interface IPlayerControllerControl
+{
+    void SetCanMove(bool value);
+    void SetCanLook(bool value);
+}
+public class PlayerMovement : MonoBehaviour, IPlayerControllerControl
+{
+
+    public float MoveSmoothTime;
+    public float GravityStrenght;
+    public float JumpStrenght;
+    public float walkSpeed;
+    public float RunSpeed;
+
+    private CharacterController Controller;
+    private Vector3 CurrentMoveVelocity;
+    private Vector3 MoveDampVelocity;
+
+    private Vector3 CurrentForceVelocity;
+
+    public bool canMove = true;
+
+
+    PlayerLook look;
+    
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+
+        look = GetComponent<PlayerLook>();
+    }
+
+
+
+    public void SetCanMove(bool value)
+    {
+        canMove = value;
+     //   Controller.enabled = value;
+        if (!canMove)
+            StopMovement();
+    }
+
+    public void SetCanLook(bool value)
+    {
+        look.canLook = value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!canMove) 
+        {
+            CurrentForceVelocity = Vector3.zero;
+            CurrentMoveVelocity = Vector3.zero;
+            Controller.Move(Vector3.zero);
+            return;
+        
+        }
+        
+        Vector3 PlayerInput = new Vector3 { x = Input.GetAxisRaw("Horizontal"), y = 0f, z = Input.GetAxisRaw("Vertical") };
+
+        if(PlayerInput.magnitude > 1f)
+        {
+            PlayerInput.Normalize();
+        }
+
+        Vector3 MoveVector = transform.TransformDirection(PlayerInput);
+        float CurrentSpeed = Input.GetKey(KeyCode.LeftShift) ? RunSpeed : walkSpeed;
+
+        CurrentMoveVelocity = Vector3.SmoothDamp(CurrentMoveVelocity, MoveVector * CurrentSpeed, ref MoveDampVelocity, MoveSmoothTime);
+
+        Controller.Move(CurrentMoveVelocity * Time.deltaTime);
+
+        Ray groundCheckRay = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(groundCheckRay, 1.1f))
+        {
+            CurrentForceVelocity.y = -2f;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                CurrentForceVelocity.y = JumpStrenght;
+            }
+
+        }
+        else
+        {
+            CurrentForceVelocity.y -= GravityStrenght * Time.deltaTime;
+        }
+
+        Controller.Move(CurrentForceVelocity * Time.deltaTime);
+
+
+
+    }
+
+    public void StopMovement()
+    {
+        CurrentMoveVelocity = Vector3.zero;
+        CurrentForceVelocity = Vector3.zero;
+    }
+}
